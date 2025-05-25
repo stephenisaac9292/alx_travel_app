75c9490475671fe8282092759c7b877b1c0168b6
Stephen Isaac <stephenisaac9292@gmail.com>
2025-05-25 22:37:40 +0100

    save10

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +79,12 @@ WSGI_APPLICATION = 'alx_travel_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'alx_travel_db',
+        'USER': 'isaac',
+        'PASSWORD': 'Connect92#',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
